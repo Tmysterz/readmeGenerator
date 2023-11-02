3a7c8e9e77661bcc0398f87cd2256c4686437e45
Terrence McCarthy <terrencemccarthysnkrs@gmail.com>
2023-11-02 17:51:16 -0500

    test

---
+++ index.js
@@ -0,0 +1,91 @@
+// TODO: Include packages needed for this application
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { generateMarkdown } = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+
+const questions = inquirer.prompt([
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "username",
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+    },
+    {
+        type: "input",
+        message: "What is your projects name?",
+        name: "projectName",
+    },
+    {
+        type: "input",
+        message: "Please write a short description of your project",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "Please write about the use of your project",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "What kind of license should your project have?",
+        name: "license",
+    },
+    // {
+    //     question: "What kind of license should your project have?",
+    //     choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+    //     name: "license",
+    // },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        name: "install",
+    },
+    {
+        type: "input",
+        message: "What command should be done to run tests?",
+        name: "tests",
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about contributing to the repo",
+        name: "contributing",
+    },
+    
+]).then(responses => {
+    console.log(responses)
+
+    writeToFile("newREADME.md", responses)
+
+
+})
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    
+    console.log(data)
+    
+    fs.writeFile(fileName, generateMarkdown(data), (err) => {
+        err ? console.error(err) : console.log('Successfully created README.md!')
+    })
+}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+
+// TO DO 
+
+// make the licnese question multiple choice with 4 answers
+// a badge for that license is added near the top of the README
+// how to add license link 
---
+++ newREADME.md
@@ -0,0 +1,53 @@
+
+  # Random Generator README.md
+  ![Github License](https://img.shields.io/badge/MIT-blue)
+
+  ## Description
+
+  this application allows the user to answer a set of questions and create a readme 
+
+  ## Table Of Contents 
+
+  * [Installation](#Installation)
+
+  * [Usage](#usage)
+  
+  * [License](#license)
+
+  * [Contributing](#contributing)
+
+  * [Tests](#tests)
+
+  * [Questions](#questions)
+
+  ## Installation
+
+  To install necessary dependencies, run the following command:
+
+  npm i
+
+  ## Usage
+
+  to save the user time and not have to fully write out a read me
+
+  ## License
+
+  This project is licensed under the MIT license undefined.
+  The license link for this project is input license link
+
+  ## Contributing 
+
+  send a pull request to git hub
+
+  ## Tests
+
+  To run tests run the following command:
+
+  npm test
+
+  ## Questions
+
+  if you have any questions about the repo, Please feel free to open a issue or contact me directly at terrencemccarthysnkrs@gmail.com
+  You can find more of my work at https://github.com/Tmysterz
+
+
